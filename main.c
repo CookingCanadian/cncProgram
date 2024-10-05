52cf10759b8f7af84cc78c39beaf7a4f89c636a9
CozyCanadian <agan0703@gmail.com>
2024-10-05 18:58:52 -0400

    op

--- main.c
+++ main.c
@@ -10,159 +10,115 @@ int main() {
     const int screenWidth = 1024;
     const int screenHeight = 600;  
 
-    //CNC Values
-    float spindleRPM = 0.0;
-    float MAXspindleRPM = 20000.0;
+    // CNC Values
+    const int MAX_SPINDLE_RPM = 20000;
+    int spindleRPM = 20000;
 
-    //int feedRateGlobal = 0;
-
-    //GUI/Program Values
-    const int deltaLerp = 5;
+    // GUI/Program Values
+    const int DELTA_LERP = 5;
     int tabHighlightYPos = 0;
     int targetTabHighlightYPos = 0;    
-    char spindleRPMText[5]; //00000
-    char spindleRPMPercentageText[7]; //000.00%
+    char spindleRPMText[6]; // Updated size to fit large numbers and null terminator
+    char spindleRPMPercentageText[8]; // 000.00%
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT);//Anti-Aliasing
-    InitWindow(screenWidth, screenHeight, "cncProgram");
+    SetConfigFlags(FLAG_MSAA_4X_HINT); // Anti-Aliasing
+    InitWindow(screenWidth, screenHeight, "CNC Program");
 
-    //Loading Fonts
-    Font regular15 = LoadFontEx("JosefinSans-Regular.ttf",15,0,0);
-    Font semiBold = LoadFontEx("JosefinSans-SemiBold.ttf",30,0,0);
-    Font bold15 = LoadFontEx("JosefinSans-Bold.ttf",15,0,0);
-    Font bold25 = LoadFontEx("JosefinSans-Bold.ttf",25,0,0);
+    // Loading Fonts
+    Font regular15 = LoadFontEx("JosefinSans-Regular.ttf", 15, 0, 0);
+    Font semiBold = LoadFontEx("JosefinSans-SemiBold.ttf", 30, 0, 0);
+    Font bold15 = LoadFontEx("JosefinSans-Bold.ttf", 15, 0, 0);
+    Font bold25 = LoadFontEx("JosefinSans-Bold.ttf", 25, 0, 0);
 
-    //Calculating Font Positions (Unchanging; 4Y Offset) 
-    int spindleTextSizeX = 125-((MeasureTextEx(bold15,"SPINDLE RPM",15,0).x)/2);
-    int fileTextSizeY = 29-((MeasureTextEx(bold25,"FILE",25,0).y)/2);
-    int probeTextSizeY = 79-((MeasureTextEx(bold25,"PROBE",25,0).y)/2);
-    int toolTextSizeY = 129-((MeasureTextEx(bold25,"TOOL",25,0).y)/2);
-    int runTextSizeY = 179-((MeasureTextEx(bold25,"RUN",25,0).y)/2);
+    // Calculating Font Positions (Unchanging; 4Y Offset) 
+    int spindleTextSizeX = 125 - (MeasureTextEx(bold15, "SPINDLE RPM", 15, 0).x / 2);
+    int fileTextSizeY = 29 - (MeasureTextEx(bold25, "FILE", 25, 0).y / 2);
+    int probeTextSizeY = 79 - (MeasureTextEx(bold25, "PROBE", 25, 0).y / 2);
+    int toolTextSizeY = 129 - (MeasureTextEx(bold25, "TOOL", 25, 0).y / 2);
+    int runTextSizeY = 179 - (MeasureTextEx(bold25, "RUN", 25, 0).y / 2);
 
-    SetTargetFPS(200);    
+    SetTargetFPS(100);    
     if (IsFontReady(bold25) && IsFontReady(bold15) && IsFontReady(semiBold) && IsFontReady(regular15)) {         
         while (!WindowShouldClose()) {        
             BeginDrawing();
             ClearBackground(RAYWHITE);        
-            //Lower Dashboard      
-            sprintf(spindleRPMText,"%.0f", spindleRPM);
-            Vector2 spindleRPMTextSize = MeasureTextEx(semiBold,spindleRPMText,30,0);
 
-            sprintf(spindleRPMPercentageText, "%.2f", 100*spindleRPM/MAXspindleRPM);          
-            Vector2 spindleRPMPercentageTextSize = MeasureTextEx(regular15,spindleRPMPercentageText,15,0);
+            // Lower Dashboard      
+            sprintf(spindleRPMText, "%d", spindleRPM); // Display RPM as a regular integer
+            Vector2 spindleRPMTextSize = MeasureTextEx(semiBold, spindleRPMText, 30, 0);
+            sprintf(spindleRPMPercentageText, "%d%%", (100 * spindleRPM) / MAX_SPINDLE_RPM); // Proper percentage calculation as int
+            Vector2 spindleRPMPercentageTextSize = MeasureTextEx(regular15, spindleRPMPercentageText, 15, 0);
+
+            DrawRectangle(0, 350, 1024, 250, (Color){43, 44, 64, 255}); // Background   
+            DrawRectangleGradientH(217, 383, 200, 50, (Color){31, 31, 44, 255}, (Color){43, 44, 64, 255}); // Feed Override            
+            DrawCircle(125, 475, 130, (Color){43, 44, 64, 255});            
+            DrawRing((Vector2){125, 475}, 125, 130, 315, 405, 40, (Color){241, 240, 255, 255}); 
+            DrawRing((Vector2){125, 475}, 70, 100, 135, 405, 40, (Color){241, 240, 255, 255}); // Spindle Override
+            DrawCircle(125, 475, 62.5, (Color){35, 36, 53, 255});            
+            DrawTextEx(bold15, "SPINDLE RPM", (Vector2){spindleTextSizeX, 560}, 15, 0, SELECTED_TEXT);  
+            DrawTextEx(regular15, spindleRPMPercentageText, (Vector2){125 - (spindleRPMPercentageTextSize.x / 2), 500}, 15, 0, TERTIARY_TEXT);
+            DrawTextEx(semiBold, spindleRPMText, (Vector2){125 - (spindleRPMTextSize.x / 2), 475 - (spindleRPMTextSize.y / 2)}, 30, 0, SELECTED_TEXT);              
+            DrawRing((Vector2){125, 475}, 70, 100, 135, (135 + (270 * spindleRPM / MAX_SPINDLE_RPM)), 40, PURPLE);
             
+            // Display Area
+            DrawRectangle(200, 0, 824, 350, (Color){148, 149, 177, 255});
 
-            DrawRectangle(0,350,1024,250,(Color){43,44,64,255}); //Background   
-            DrawRectangleGradientH(217,383,200,50,(Color){31,31,44,255},(Color){43,44,64,255}); //Feed Override            
-            DrawCircle(125,475,130,(Color){43,44,64,255});            
-            DrawRing((Vector2){125,475},125,130,315,405,40,(Color){241,240,255,255}); 
-            DrawRing((Vector2){125,475},70,100,135,405,40,(Color){241,240,255,255}); //Spindle Override
-            DrawCircle(125,475,62.5,(Color){35,36,53,255});            
-            DrawTextEx(bold15,"SPINDLE RPM",(Vector2){spindleTextSizeX,560},15,0,SELECTED_TEXT);  
-            DrawTextEx(regular15,spindleRPMPercentageText,(Vector2){125-(spindleRPMPercentageTextSize.x/2),500},15,0,TERTIARY_TEXT);
-            DrawTextEx(semiBold,spindleRPMText,(Vector2){125-(spindleRPMTextSize.x/2),475-(spindleRPMTextSize.y/2)},30,0,SELECTED_TEXT);              
-            DrawRing((Vector2){125,475},70,100,135,(135+(270*spindleRPM/MAXspindleRPM)),40,PURPLE);
+            // Side Tabs
+            DrawRectangle(0, 0, 200, 350, (Color){82, 83, 109, 255});         
+            DrawRectangleGradientH(0, tabHighlightYPos, 200, 50, (Color){82, 83, 109, 255}, (Color){148, 149, 177, 255}); // Highlight Icon
             
-            //Display Area
-            DrawRectangle(200,0,824,350,(Color){148,149,177,255});
-
-            //Side Tabs
-            DrawRectangle(0,0,200,350,(Color){82,83,109,255});         
-            DrawRectangleGradientH(0,tabHighlightYPos,200,50,(Color){82,83,109,255},(Color){148,149,177,255}); //Highlight Icon
-            switch(tabHighlightYPos) { //Color Animation
-                case 0:
-                    DrawTextEx(bold25,"FILE",(Vector2){20,fileTextSizeY},25,0,SELECTED_TEXT); 
-                    DrawTextEx(bold25,"PROBE",(Vector2){20,probeTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"TOOL",(Vector2){20,toolTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"RUN",(Vector2){20,runTextSizeY},25,0,UNSELECTED_TEXT);
-                    break;
-                case 50:
-                    DrawTextEx(bold25,"FILE",(Vector2){20,fileTextSizeY},25,0,UNSELECTED_TEXT); 
-                    DrawTextEx(bold25,"PROBE",(Vector2){20,probeTextSizeY},25,0,SELECTED_TEXT);
-                    DrawTextEx(bold25,"TOOL",(Vector2){20,toolTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"RUN",(Vector2){20,runTextSizeY},25,0,UNSELECTED_TEXT);
-                    break;
-                case 100:
-                    DrawTextEx(bold25,"FILE",(Vector2){20,fileTextSizeY},25,0,UNSELECTED_TEXT); 
-                    DrawTextEx(bold25,"PROBE",(Vector2){20,probeTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"TOOL",(Vector2){20,toolTextSizeY},25,0,SELECTED_TEXT);
-                    DrawTextEx(bold25,"RUN",(Vector2){20,runTextSizeY},25,0,UNSELECTED_TEXT);
-                    break;
-                case 150:
-                    DrawTextEx(bold25,"FILE",(Vector2){20,fileTextSizeY},25,0,UNSELECTED_TEXT); 
-                    DrawTextEx(bold25,"PROBE",(Vector2){20,probeTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"TOOL",(Vector2){20,toolTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"RUN",(Vector2){20,runTextSizeY},25,0,SELECTED_TEXT);
-                    break;
-                default:
-                    DrawTextEx(bold25,"FILE",(Vector2){20,fileTextSizeY},25,0,UNSELECTED_TEXT); 
-                    DrawTextEx(bold25,"PROBE",(Vector2){20,probeTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"TOOL",(Vector2){20,toolTextSizeY},25,0,UNSELECTED_TEXT);
-                    DrawTextEx(bold25,"RUN",(Vector2){20,runTextSizeY},25,0,UNSELECTED_TEXT);
+            // Tab color animation
+            const char* tabNames[] = {"FILE", "PROBE", "TOOL", "RUN"};
+            int tabPositionsY[] = {fileTextSizeY, probeTextSizeY, toolTextSizeY, runTextSizeY};
+            for (int i = 0; i < 4; i++) {
+                Color textColor = (tabHighlightYPos == i * 50) ? SELECTED_TEXT : UNSELECTED_TEXT;
+                DrawTextEx(bold25, tabNames[i], (Vector2){20, tabPositionsY[i]}, 25, 0, textColor);
             }
-            DrawLineEx((Vector2){0,50},(Vector2){200,50},1,(Color){58,59,84,255});     
-            DrawLineEx((Vector2){0,100},(Vector2){200,100},1,(Color){58,59,84,255});          
-            DrawLineEx((Vector2){0,150},(Vector2){200,150},1,(Color){58,59,84,255}); 
 
-            //Mouse Handler       
+            // Draw Tab Separators
+            for (int i = 1; i < 4; i++) {
+                DrawLineEx((Vector2){0, i * 50}, (Vector2){200, i * 50}, 1, (Color){58, 59, 84, 255});     
+            }
+
+            // Mouse Handler       
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                 Vector2 mousePosition = GetMousePosition();
-                if (mousePosition.x <= 200 && mousePosition.y < 350) { //Side Tab Area
-                    if (mousePosition.y >=0 && mousePosition.y < 50) { //Button 1
-                        targetTabHighlightYPos = 0;
-                    } else if (mousePosition.y >= 50 && mousePosition.y < 100) { //Button 2
-                        targetTabHighlightYPos = 50;
-                    } else if (mousePosition.y >=100 && mousePosition.y < 150) { //Button 3
-                        targetTabHighlightYPos = 100;
-                    } else if (mousePosition.y >=150 && mousePosition.y < 200) { //Button 4
-                        targetTabHighlightYPos = 150;
+                if (mousePosition.x <= 200 && mousePosition.y < 350) { // Side Tab Area
+                    int clickedTab = mousePosition.y / 50; // Determine which tab was clicked
+                    if (clickedTab >= 0 && clickedTab < 4) {
+                        targetTabHighlightYPos = clickedTab * 50;
                     }
                 }
             }
 
-            //Touch Handler           
-            
-
-            //GUI Animation
-            if (tabHighlightYPos != targetTabHighlightYPos) {
-                if (tabHighlightYPos > targetTabHighlightYPos) {
-                    tabHighlightYPos -= deltaLerp;
-                } else if (tabHighlightYPos < targetTabHighlightYPos) {
-                    tabHighlightYPos += deltaLerp;
-                }
+            // Smoothing Animations
+            if (tabHighlightYPos < targetTabHighlightYPos) {
+                tabHighlightYPos += DELTA_LERP;
+            } else if (tabHighlightYPos > targetTabHighlightYPos) {
+                tabHighlightYPos -= DELTA_LERP;
             }
 
-            //Testing Items
+            // Testing Items (Mouse Scroll to Change RPM)
             int mouseScroll = (int)GetMouseWheelMove();
             if (mouseScroll > 0) {
-                if (spindleRPM < MAXspindleRPM) {
-                    spindleRPM += mouseScroll*500;
+                if (spindleRPM < MAX_SPINDLE_RPM) {
+                    spindleRPM += mouseScroll * 500;
                 }
             } else {
                 if (spindleRPM >= 1) {
-                    spindleRPM += mouseScroll*500;
+                    spindleRPM += mouseScroll * 500;
                 }
-            }              
+            }    
 
-            if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT))) {              
-                int display = GetCurrentMonitor();    
-                
-                if (IsWindowFullscreen())
-                {                   
-                    SetWindowSize(screenWidth, screenHeight);
-                }
-                else
-                {                  
-                    SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
-                }   
-                ToggleFullscreen();
-            }
-
-            DrawFPS(512,300);
             EndDrawing();
         }
     }
+    
+    UnloadFont(regular15);
+    UnloadFont(semiBold);
+    UnloadFont(bold15);
+    UnloadFont(bold25);
     CloseWindow();
+    
     return 0;
 }
-
